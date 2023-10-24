7197cbb52c01dd8e49b731f11a73e536e1145870
RLRRLRLL <diddleman@icloud.com>
2023-10-24 18:51:06 +0300

    UI, sfxDelay, msgDelay

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -4,13 +4,13 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
-import java.net.URL;
+
 
 public class Sound {
 
     Clip clip;
     File[] sounds = new File[6];
-   // URL[] soundURL =new URL[30];
+    /* URL[] soundURL =new URL[30]; */
 
     public Sound(){
 
@@ -21,12 +21,6 @@ public class Sound {
         sounds[3] = new File("src/sound/close.wav");
         sounds[4] = new File("src/sound/grub.wav");
         sounds[5] = new File("src/sound/win.wav");
-//        soundURL[0] = getClass().getResource("/sound/epic .wav");
-//        soundURL[1] = getClass().getResource("/sound/coin.wav");
-//        soundURL[2] = getClass().getResource("/sound/unlock.wav");
-//        soundURL[3] = getClass().getResource("/sound/close.wav");
-//        soundURL[4] = getClass().getResource("/sound/grub.wav");
-//        soundURL[5] = getClass().getResource("/sound/win.wav");
 
 
 
