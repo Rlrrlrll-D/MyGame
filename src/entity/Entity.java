ace9f24691f649c9309d6f56973fb69a6fd1a47d
Rlrrlrll-D <diddleman@icloud.com>
2023-11-14 17:30:29 +0200

    throw ..(Exception)

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -15,7 +15,7 @@ public class Entity {
     public int worldX, worldY;
     public int speed;
 
-    public BufferedImage stay1, stay2, stay3, stay_up1, stay_up2, stay_up3,
+    public BufferedImage shadow, stay1, stay2, stay3, stay_up1, stay_up2, stay_up3,
             stay_left1, stay_left2, stay_left3, stay_right1, stay_right2, stay_right3, up1, up2, down1, down2, left1, left2, right1, right2;
     public String direct, stayDirect;
 
@@ -219,8 +219,8 @@ public class Entity {
                         image = stay_right3;
                     }
                     break;
-
-
+                default:
+                    throw new IllegalStateException("Unexpected value: " + direct);
             }
             Color shadow = new Color(12, 12, 12, 55);
             graphics2D.setColor(shadow);
