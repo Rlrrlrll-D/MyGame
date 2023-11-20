40224c7e906ccbf40f52efa0c33f9df52828a578
RLRRLRLL <diddleman@icloud.com>
2023-11-20 09:11:53 +0200

    Main class fix

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,18 +8,17 @@ public class Main {
     public static void main(String[] args) throws IOException, FontFormatException {
 
         JFrame window = new JFrame();
-
-        window.setLocationRelativeTo(null);
-        window.setTitle("My Hero Adventure");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("My Hero Adventure");
 
 
         GamePanel gamePanel = new GamePanel();
-
-        window.setVisible(true);
-        window.setResizable(false);
         window.add(gamePanel);
         window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
         gamePanel.setupGame();
         gamePanel.startGameThread();
