17ab14dc3eb27ce2890ffd5f767c8c0a393d4cfb
Rlrrlrll-D <diddleman@icloud.com>
2024-03-24 13:13:48 +0200

    Refactoring the save process

--- src/data/SaveLoad.java
+++ src/data/SaveLoad.java
@@ -4,7 +4,6 @@ import main.GamePanel;
 
 import java.io.*;
 
-
 public class SaveLoad {
 
     GamePanel gamePanel;
@@ -13,57 +12,62 @@ public class SaveLoad {
         this.gamePanel = gamePanel;
     }
 
-
-
     public void save() {
         try {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("save.dat"));
             DataStorage dataStorage = new DataStorage();
-            dataStorage.level = gamePanel.player.level;
-            dataStorage.maxLife = gamePanel.player.maxLife;
-            dataStorage.life = gamePanel.player.life;
-            dataStorage.maxMana = gamePanel.player.maxMana;
-            dataStorage.mana = gamePanel.player.mana;
-            dataStorage.strength = gamePanel.player.strength;
-            dataStorage.dexterity = gamePanel.player.dexterity;
-            dataStorage.exp = gamePanel.player.exp;
-            dataStorage.nextLevelExp = gamePanel.player.nextLevelExp;
-            dataStorage.coin = gamePanel.player.coin;
-            for (int i = 0; i < gamePanel.player.inventory.size(); i++) {
-
-                dataStorage.itemNames.add(gamePanel.player.inventory.get(i).name);
-                dataStorage.itemAmounts.add(gamePanel.player.inventory.get(i).amount);
-
-            }
-
-            dataStorage.currentWeaponSlot = gamePanel.player.getCurrentWeaponSlot();
-            dataStorage.currentShieldSlot = gamePanel.player.getCurrentShieldSlot();
-
-            dataStorage.mapObjNames = new String[GamePanel.maxMap][gamePanel.objects[1].length];
-            dataStorage.objWorldX = new int[GamePanel.maxMap][gamePanel.objects[1].length];
-            dataStorage.objWorldY = new int[GamePanel.maxMap][gamePanel.objects[1].length];
-            dataStorage.lootNames = new String[GamePanel.maxMap][gamePanel.objects[1].length];
-            dataStorage.objOpened = new boolean[GamePanel.maxMap][gamePanel.objects[1].length];
-
-            for (int i = 0; i < GamePanel.maxMap; i++) {
-                for (int j = 0; j < gamePanel.objects[1].length; j++) {
-                    if (gamePanel.objects[i][j] == null) {
-                        dataStorage.mapObjNames[i][j] = "NA";
-                    } else {
-                        dataStorage.mapObjNames[i][j] = gamePanel.objects[i][j].name;
-                        dataStorage.objWorldX[i][j] = gamePanel.objects[i][j].worldX;
-                        dataStorage.objWorldY[i][j] = gamePanel.objects[i][j].worldY;
-                        if (gamePanel.objects[i][j].loot != null) {
-                            dataStorage.lootNames[i][j] = gamePanel.objects[i][j].loot.name;
-                        }
-                        dataStorage.objOpened[i][j] = gamePanel.objects[i][j].opened;
-                    }
-                }
-            }
+            savePlayerData(dataStorage);
+            saveInventoryData(dataStorage);
+            saveObjectData(dataStorage);
             objectOutputStream.writeObject(dataStorage);
-
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void savePlayerData(DataStorage dataStorage) {
+        dataStorage.level = gamePanel.player.level;
+        dataStorage.maxLife = gamePanel.player.maxLife;
+        dataStorage.life = gamePanel.player.life;
+        dataStorage.maxMana = gamePanel.player.maxMana;
+        dataStorage.mana = gamePanel.player.mana;
+        dataStorage.strength = gamePanel.player.strength;
+        dataStorage.dexterity = gamePanel.player.dexterity;
+        dataStorage.exp = gamePanel.player.exp;
+        dataStorage.nextLevelExp = gamePanel.player.nextLevelExp;
+        dataStorage.coin = gamePanel.player.coin;
+    }
+
+    private void saveInventoryData(DataStorage dataStorage) {
+        for (int i = 0; i < gamePanel.player.inventory.size(); i++) {
+            dataStorage.itemNames.add(gamePanel.player.inventory.get(i).name);
+            dataStorage.itemAmounts.add(gamePanel.player.inventory.get(i).amount);
+        }
+        dataStorage.currentWeaponSlot = gamePanel.player.getCurrentWeaponSlot();
+        dataStorage.currentShieldSlot = gamePanel.player.getCurrentShieldSlot();
+    }
+
+    private void saveObjectData(DataStorage dataStorage) {
+        dataStorage.mapObjNames = new String[GamePanel.maxMap][gamePanel.objects[1].length];
+        dataStorage.objWorldX = new int[GamePanel.maxMap][gamePanel.objects[1].length];
+        dataStorage.objWorldY = new int[GamePanel.maxMap][gamePanel.objects[1].length];
+        dataStorage.lootNames = new String[GamePanel.maxMap][gamePanel.objects[1].length];
+        dataStorage.objOpened = new boolean[GamePanel.maxMap][gamePanel.objects[1].length];
+
+        for (int i = 0; i < GamePanel.maxMap; i++) {
+            for (int j = 0; j < gamePanel.objects[1].length; j++) {
+                if (gamePanel.objects[i][j] == null) {
+                    dataStorage.mapObjNames[i][j] = "NA";
+                } else {
+                    dataStorage.mapObjNames[i][j] = gamePanel.objects[i][j].name;
+                    dataStorage.objWorldX[i][j] = gamePanel.objects[i][j].worldX;
+                    dataStorage.objWorldY[i][j] = gamePanel.objects[i][j].worldY;
+                    if (gamePanel.objects[i][j].loot != null) {
+                        dataStorage.lootNames[i][j] = gamePanel.objects[i][j].loot.name;
+                    }
+                    dataStorage.objOpened[i][j] = gamePanel.objects[i][j].opened;
+                }
+            }
         }
     }
 
@@ -71,6 +75,12 @@ public class SaveLoad {
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("save.dat"));
         DataStorage dataStorage = (DataStorage) objectInputStream.readObject();
 
+        loadPlayerData(dataStorage);
+        loadInventoryData(dataStorage);
+        loadObjectData(dataStorage);
+    }
+
+    private void loadPlayerData(DataStorage dataStorage) {
         gamePanel.player.level = dataStorage.level;
         gamePanel.player.maxLife = dataStorage.maxLife;
         gamePanel.player.life = dataStorage.life;
@@ -81,20 +91,23 @@ public class SaveLoad {
         gamePanel.player.exp = dataStorage.exp;
         gamePanel.player.nextLevelExp = dataStorage.nextLevelExp;
         gamePanel.player.coin = dataStorage.coin;
+    }
 
+    private void loadInventoryData(DataStorage dataStorage) {
         gamePanel.player.inventory.clear();
 
         for (int i = 0; i < dataStorage.itemNames.size(); i++) {
-
             gamePanel.player.inventory.add(gamePanel.entityGenerator.getObj(dataStorage.itemNames.get(i)));
             gamePanel.player.inventory.get(i).amount = dataStorage.itemAmounts.get(i);
-
         }
         gamePanel.player.currentWeapon = gamePanel.player.inventory.get(dataStorage.currentWeaponSlot);
         gamePanel.player.currentShield = gamePanel.player.inventory.get(dataStorage.currentShieldSlot);
         gamePanel.player.getAttack();
         gamePanel.player.getDefence();
         gamePanel.player.getAttackImage();
+    }
+
+    private void loadObjectData(DataStorage dataStorage) {
         for (int i = 0; i < GamePanel.maxMap; i++) {
             for (int j = 0; j < gamePanel.objects[1].length; j++) {
                 if (dataStorage.mapObjNames[i][j].equals("NA")) {
