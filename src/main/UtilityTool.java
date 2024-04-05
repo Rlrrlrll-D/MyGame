1ed8cf052026756c429ae64ea6b54a3d500e930c
Rlrrlrll-D <diddleman@icloud.com>
2024-04-05 19:00:27 +0300

    Sprites

--- src/main/UtilityTool.java
+++ src/main/UtilityTool.java
@@ -5,15 +5,12 @@ import java.awt.image.BufferedImage;
 
 public class UtilityTool {
 
-
     public static BufferedImage scaleImage(BufferedImage original, int width, int height) {
 
         BufferedImage scaledImage = new BufferedImage(width, height, original.getType());
         Graphics2D graphics2D = scaledImage.createGraphics();
         graphics2D.drawImage(original, 0, 0, width, height, null);
         graphics2D.dispose();
-
         return scaledImage;
-
     }
 }
