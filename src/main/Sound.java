9aae858e068ba3b6586fe0b83f515ee5705e7389
RLRRLRLL <diddleman@icloud.com>
2024-05-09 11:22:39 +0300

    Release 1.0

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -4,52 +4,52 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
-import java.io.File;
+import java.net.URL;
 
 public class Sound {
 
     Clip clip;
-    File[] sounds = new File[40];
+    URL[] soundURL = new URL[40];
     FloatControl floatControl;
     public int volumeScale = 2;
     float volume;
 
     public Sound() {
 
-        sounds[0] = new File("src/res/sound/Joshua McLean - Mountain Trials.wav");
-        sounds[1] = new File("src/res/sound/coin.wav");
-        sounds[2] = new File("src/res/sound/unlock.wav");
-        sounds[3] = new File("src/res/sound/close.wav");
-        sounds[4] = new File("src/res/sound/grub.wav");
-        sounds[5] = new File("src/res/sound/win.wav");
-        sounds[6] = new File("src/res/sound/ouch.wav");
-        sounds[7] = new File("src/res/sound/hit-monster.wav");
-        sounds[8] = new File("src/res/sound/receive-damage.wav");
-        sounds[9] = new File("src/res/sound/swing-weapon.wav");
-        sounds[10] = new File("src/res/sound/health.wav");
-        sounds[11] = new File("src/res/sound/leve-lup.wav");
-        sounds[12] = new File("src/res/sound/cursor.wav");
-        sounds[13] = new File("src/res/sound/yahoo.wav");
-        sounds[14] = new File("src/res/sound/burning.wav");
-        sounds[15] = new File("src/res/sound/cut-tree.wav");
-        sounds[16] = new File("src/res/sound/game-over.wav");
-        sounds[17] = new File("src/res/sound/sleep.wav");
-        sounds[18] = new File("src/res/sound/blocked.wav");
-        sounds[19] = new File("src/res/sound/parry.wav");
-        sounds[20] = new File("src/res/sound/speak.wav");
-        sounds[21] = new File("src/res/sound/Kevin MacLeod - 8bit Dungeon Level.wav");
-        sounds[22] = new File("src/res/sound/merchant.wav");
-        sounds[23] = new File("src/res/sound/chip-wall.wav");
-        sounds[24] = new File("src/res/sound/door-open.wav");
-        sounds[25] = new File("src/res/sound/FinalBattle.wav");
-        sounds[26] = new File("src/res/sound/fanfare.wav");
-        sounds[27] = new File("src/res/sound/slime.wav");
+        soundURL[0] = getClass().getResource("/res/sound/Joshua McLean - Mountain Trials.wav");
+        soundURL[1] = getClass().getResource("/res/sound/coin.wav");
+        soundURL[2] = getClass().getResource("/res/sound/unlock.wav");
+        soundURL[3] = getClass().getResource("/res/sound/close.wav");
+        soundURL[4] = getClass().getResource("/res/sound/grub.wav");
+        soundURL[5] = getClass().getResource("/res/sound/win.wav");
+        soundURL[6] = getClass().getResource("/res/sound/ouch.wav");
+        soundURL[7] = getClass().getResource("/res/sound/hit-monster.wav");
+        soundURL[8] = getClass().getResource("/res/sound/receive-damage.wav");
+        soundURL[9] = getClass().getResource("/res/sound/swing-weapon.wav");
+        soundURL[10] = getClass().getResource("/res/sound/health.wav");
+        soundURL[11] = getClass().getResource("/res/sound/leve-lup.wav");
+        soundURL[12] = getClass().getResource("/res/sound/cursor.wav");
+        soundURL[13] = getClass().getResource("/res/sound/yahoo.wav");
+        soundURL[14] = getClass().getResource("/res/sound/burning.wav");
+        soundURL[15] = getClass().getResource("/res/sound/cut-tree.wav");
+        soundURL[16] = getClass().getResource("/res/sound/game-over.wav");
+        soundURL[17] = getClass().getResource("/res/sound/sleep.wav");
+        soundURL[18] = getClass().getResource("/res/sound/blocked.wav");
+        soundURL[19] = getClass().getResource("/res/sound/parry.wav");
+        soundURL[20] = getClass().getResource("/res/sound/speak.wav");
+        soundURL[21] = getClass().getResource("/res/sound/Kevin MacLeod - 8bit Dungeon Level.wav");
+        soundURL[22] = getClass().getResource("/res/sound/merchant.wav");
+        soundURL[23] = getClass().getResource("/res/sound/chip-wall.wav");
+        soundURL[24] = getClass().getResource("/res/sound/door-open.wav");
+        soundURL[25] = getClass().getResource("/res/sound/FinalBattle.wav");
+        soundURL[26] = getClass().getResource("/res/sound/fanfare.wav");
+        soundURL[27] = getClass().getResource("/res/sound/slime.wav");
 
     }
 
     public void setFile(int count) {
         try {
-            AudioInputStream audio = AudioSystem.getAudioInputStream(sounds[count]);
+            AudioInputStream audio = AudioSystem.getAudioInputStream(soundURL[count]);
             clip = AudioSystem.getClip();
             clip.open(audio);
             floatControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
@@ -72,10 +72,10 @@ public class Sound {
     }
 
     public void chkVolume() {
-    float[] volumeLevels = {-80f, -20f, -12f, -5f, 1f, 6f};
-    if (volumeScale >= 0 && volumeScale < volumeLevels.length) {
-        volume = volumeLevels[volumeScale];
+        float[] volumeLevels = {-80f, -20f, -12f, -5f, 1f, 6f};
+        if (volumeScale >= 0 && volumeScale < volumeLevels.length) {
+            volume = volumeLevels[volumeScale];
+        }
+        floatControl.setValue(volume);
     }
-    floatControl.setValue(volume);
-}
 }
